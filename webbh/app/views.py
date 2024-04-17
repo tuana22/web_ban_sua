4ec5c780d1e8d88f953d583081ea5d9ff7aa71eb
tuana22 <225130051@gmail.com>
2024-04-17 13:13:09 +0700

    first commit

---
+++ webbh/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from .models import *
+
+# Create your views here.
+
+def home(request):
+    products= product.objects.all()
+    context= {'products':products}
+    return render( request, 'app/home.html',context)
+def cart(request):
+    context= {}
+    return render( request, 'app/cart.html',context)
+def checkout(request):
+    context= {}
+    return render( request, 'app/checkout.html',context)
